4b8acfc217c9abc27202e68544bccf87f9a816c0
Baha <hakanbahadircelik@gmail.com>
2025-03-03 17:19:48 +0400

    priority and testNG writing sections

--- src/_01_Introduction/_01_Explanation.java
+++ src/_01_Introduction/_01_Explanation.java
@@ -1,21 +1,29 @@
 package _01_Introduction;
 
+import org.testng.annotations.Test;
+
 public class _01_Explanation {
 
+    // it works algoritmic on default
+
+    @Test(priority = 1)
     public void driverStart(){
-
+        System.out.println("driver started");
     }
 
+    @Test(priority = 2)
     public void openWebsite(){
-
+        System.out.println("Website opened");
     }
 
+    @Test(priority = 3)
     public void loginTest(){
-
+        System.out.println("login test passed");
     }
 
+    @Test(priority = 4)
     public void closeDriver(){
-
+        System.out.println("driver closed");
     }
 
 }
