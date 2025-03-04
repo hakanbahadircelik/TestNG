da8f6bd3cb58b9f934f3e51efabd655365bc5e39
Baha <hakanbahadircelik@gmail.com>
2025-03-04 15:36:55 +0400

    small example

--- src/_02_EnableDisable/_02_Example.java
+++ src/_02_EnableDisable/_02_Example.java
@@ -1,13 +1,29 @@
 package _02_EnableDisable;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class _02_Example extends BaseDriver {
 
     @Test
     public void test01(){
-        System.out.println("my test is worked");
+
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+
+        WebElement email = driver.findElement(By.xpath("//input[@name='email']"));
+        email.sendKeys("bahasteel@gmail.com");
+
+        WebElement passw = driver.findElement(By.xpath("//input[@name='password']"));
+        passw.sendKeys("555667788");
+
+        WebElement loign = driver.findElement(By.xpath("//input[@type='submit']"));
+        loign.click();
+
+        Assert.assertTrue(driver.getTitle().equals("My account"));
+
     }
 
 }
