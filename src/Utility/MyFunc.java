3e9fd6fb432a94f7d207d68aa73b1f061fe000ba
Baha <hakanbahadircelik@gmail.com>
2025-03-04 15:58:20 +0400

    successMessageValidation confirmation

--- src/Utility/MyFunc.java
+++ src/Utility/MyFunc.java
@@ -1,5 +1,9 @@
 package Utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 public class MyFunc {
 
     public static void Wait(int sec){
@@ -8,6 +12,12 @@ public class MyFunc {
         } catch (InterruptedException e){
             throw new RuntimeException(e);
         }
+    }
+
+    // we use this confirmation several times, so I wrote here to use again for anytime
+    public static void successMessageValidation(){
+        WebElement msgLabel = BaseDriver.driver.findElement(By.xpath(""));
+        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
     }
 
 }
