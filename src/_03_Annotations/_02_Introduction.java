1a5d94e3bde58db156a59e18430f6762e1e586af
Baha <hakanbahadircelik@gmail.com>
2025-03-10 15:13:03 +0400

    XML testing and installed

---
+++ src/_03_Annotations/_02_Introduction.java
@@ -0,0 +1,14 @@
+package _03_Annotations;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _02_Introduction extends BaseDriver {
+
+    @Test
+    public void Intro02_Test01(){ System.out.println("intro 2 - test 1"); }
+
+    @Test
+    public void Intro02_Test02(){ System.out.println("intro 2 - test 2"); }
+
+}
