322ccf087bb1f963620790f03be8a05068fe72a2
Baha <hakanbahadircelik@gmail.com>
2025-03-15 02:26:17 +0400

    dataProvider small add

--- src/_05_DataProvider/_01_Intro.java
+++ src/_05_DataProvider/_01_Intro.java
@@ -19,5 +19,17 @@ public class _01_Intro {
         return searches;
     }
 
+    /*------------------------------------------------------------*/
+
+    @Test(dataProvider = "datas")
+    public void test02(int data){
+        System.out.println("data = " + data);
+    }
+
+    @DataProvider(name="datas") // you can give a name
+    Object[] getData(){
+        Object[] data={1,2,3,4,5,6};
+        return data;
+    }
 
 }
