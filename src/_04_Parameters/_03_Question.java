69dc15ad01769d5925b6f674342a1395e3892afa
Baha <hakanbahadircelik@gmail.com>
2025-03-15 01:59:18 +0400

    answer of this question

---
+++ src/_04_Parameters/_03_Question.java
@@ -0,0 +1,35 @@
+package _04_Parameters;
+
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _03_Question extends BaseDriver {
+
+    @Test
+    public void test01() {
+
+        WebElement special = driver.findElement(By.xpath("//a[text()='Specials']"));
+        special.click();
+
+        List<WebElement> priceNew = driver.findElements(By.xpath("//span[@class='price-new']"));
+        List<WebElement> priceOld = driver.findElements(By.xpath("//span[@class='price-old']"));
+
+        Assert.assertTrue(priceNew.size() == priceOld.size(), "doesnt have discount on all of them");
+
+        for (int i = 0; i < priceNew.size(); i++) {
+            double newPrice = Double.parseDouble(priceNew.get(i).getText().replaceAll("[^0-9.,]", ""));
+            double oldPrice = Double.parseDouble(priceOld.get(i).getText().replaceAll("[^0-9.,]", ""));
+
+            System.out.println(newPrice + " " + oldPrice);
+            Assert.assertTrue(newPrice < oldPrice,"new price is not smaller than old price");
+        }
+
+
+    }
+
+}
